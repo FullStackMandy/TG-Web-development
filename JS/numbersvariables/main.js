956968a1aa563c11b9b4ccd0448307137c101f40
Mandy <FullStackMandy@gmail.com>
2020-10-04 18:43:37 +0200

    first js assignments

---
+++ JS/numbersvariables/main.js
@@ -0,0 +1,89 @@
+// Undefined en getallen aan variabelen toekennen //
+// Opdracht 1 //
+let getal1 = "";
+
+// Opdracht 2 //
+const getal2 = "2";
+
+// Opdracht 3 //
+let getal3 = '3';
+
+// Opdracht 4 //
+getal3 = "5";
+console.log(getal3);
+let datatype = "true";
+console.log(datatype + typeof datatype);
+
+// Verschillende data types herkennen opdracht 5 en 6 zie ook html//
+
+// Wat kan je met numbers doen //
+// Opdracht 7 //
+var price1 = 6;
+var price2 = 8;
+var total = price1 + price2;
+document.getElementById("demo").innerHTML =
+   "The total is: " + total;
+console.log(total);
+
+var price1 = 8;
+var price2 = 6;
+var total = price1 - price2;
+document.getElementById("demo2").innerHTML =
+   "The total is: " + total;
+
+var price1 = 24;
+var price2 = 2;
+var total = price1 * price2;
+document.getElementById("demo3").innerHTML =
+   "The total is: " + total;
+
+var price1 = 2.66;
+var price2 = 2;
+var total = price1 / price2;
+document.getElementById("demo4").innerHTML =
+   "The total is: " + total;
+
+// Opdracht 8 //
+var price1 = 3 + 5;
+var price2 = 10 / 2;
+var total = price1 * price2;
+document.getElementById("demo5").innerHTML =
+   "The total is: " + total;
+
+// Increment en decrement //
+// Opdracht 9 //
+let number = 3;
+number++;
+console.log(number++)
+
+// Opdracht 10 en 11 zijn rekensommetjes zie html 
+
+// Wat kan je met sring doen //
+// Opdracht 12 //
+var vnaam1 = "Anna";
+var anaam2 = "Bakker";
+var output = "vnaam" + "anaam";
+document.getElementById("demo6").innerHTML =
+   "The output is: " + output;
+
+// Opdracht 13 //
+var cijfer1 = "3";
+var cijfer2 = 3;
+var combinedText = "3" + 3;
+document.getElementById("demo7").innerHTML =
+   "Combined text is: " + combinedText;
+
+// Opdracht 14 zie html //
+
+
+// Opdracht 15 //
+let text = "text";
+text += "another text";
+console.log(text)
+
+
+
+// Built in methods //
+// Opdracht 16 zie html//
+
+
